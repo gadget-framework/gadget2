2cfa8e3cdceef8043d7ec1936fe270dc43c6c0db
james <james@51816742-9f83-410d-85ed-e7bcf7042953>
2003-05-09 15:33:14 +0000

    added accidentally removed files

---
+++ stockpreyptrvector.h
@@ -0,0 +1,28 @@
+#ifndef stockpreyptrvector_h
+#define stockpreyptrvector_h
+
+class StockPrey;
+
+class StockPreyptrvector {
+public:
+  StockPreyptrvector() { size = 0; v = 0; };
+  StockPreyptrvector(int sz);
+  StockPreyptrvector(int sz, StockPrey* initial);
+  StockPreyptrvector(const StockPreyptrvector& initial);
+  ~StockPreyptrvector();
+  void resize(int add, StockPrey* value);
+  void resize(int add);
+  void Delete(int pos);
+  int Size() const { return size; };
+  StockPrey*& operator [] (int pos);
+  StockPrey* const& operator [] (int pos) const;
+protected:
+  StockPrey** v;
+  int size;
+};
+
+#ifdef GADGET_INLINE
+#include "stockpreyptrvector.icc"
+#endif
+
+#endif
