8ef6b09ab0bdfca14588b810b14ef32f88911903
james <james@51816742-9f83-410d-85ed-e7bcf7042953>
2004-06-28 11:09:18 +0000

    Updates to the BFGS search algorithm

--- bfgs.cc
+++ bfgs.cc
@@ -14,42 +14,81 @@ int OptInfoBfgs::iteration(double* x0) {
   double h[NUMVARS];
   double y[NUMVARS];
   double By[NUMVARS];
-  double hy = 0.0, yBy = 0.0, alpha = 1, normgrad, temp, normdeltax;
-  int i, j, k = 0, check = 0, offset = FuncEval;
+  double hy, yBy, alpha, normgrad, normdeltax, temphy, tempyby;
+  int i, j, k, check, offset;
+  gradacc = 0.0001; //will divide by 10 later ...
 
   fk = (*f)(x0, numvar);
+  offset = FuncEval;
   gradient(x0, fk);
   for (i = 0; i < numvar; i++) {
     g0[i] = gk[i];
     x[i] = x0[i];
   }
 
-  while (k < bfgsiter) {
-    k++;
+  check = 0;
+  k = 0;
+  alpha = 1.0;
+
+  if (fk != fk) { //check for NaN
+    cout << "\nError starting BFGS optimisation with"
+      << " f(x) = infinity\nReturning to calling routine ...\n";
+    return 0;
+  }
+
+  while (1) {
+    if (isZero(fk)) {
+      cout << "\nError in BFGS optimisation after " << (FuncEval - offset)
+       << " function evaluations f(x) = 0\nReturning to calling routine ...\n";
+      return 0;
+    }
+
+    //If too many function evaluations occur, terminate the algorithm
+    if ((FuncEval - offset) > bfgsiter) {
+      cout << "\nStopping BFGS optimisation algorithm\n\n"
+        << "The optimisation stopped after " << (FuncEval - offset)
+        << " function evaluations (max " << bfgsiter << ")\nThe optimisation stopped because the "
+        << "maximum number of function evaluations\nwas reached and NOT because an "
+        << "optimum was found for this run\n";
+
+cout << "\nBFGS search was restarted " << k << " times and gradacc was reduced to " << gradacc << endl;
+
+      fk = (*f)(x, numvar);
+      EcoSystem->setFuncEvalBFGS(FuncEval - offset);
+      EcoSystem->setLikelihoodBFGS(fk);
+      return 0;
+    }
 
     if (check == 0 || isZero(alpha)) {
-      check = 1;
-      for (i = 0; i < numvar; i++) { // bfgs algoritm is restarted
-	for (j = 0; j < numvar; j++)
-	  Bk[i][j] = 0.0;
-	Bk[i][i] = 1.0;
+      if (k != 0)
+        cerr << "\nWarning in BFGS search - resetting algorithm after "
+          << (FuncEval - offset) << " function evaluations" << endl;
+      for (i = 0; i < numvar; i++) {
+        for (j = 0; j < numvar; j++)
+          Bk[i][j] = 0.0;
+        Bk[i][i] = 1.0;
       }
+      check = 1;
+      k++;
+      gradacc *= 0.1;
+      if (gradacc < rathersmall)
+        gradacc = rathersmall;
     }
 
     for (i = 0; i < numvar; i++) {
       s[i] = 0.0;
       for (j = 0; j < numvar; j++)
-	s[i] -= Bk[i][j]*gk[j];
+        s[i] -= Bk[i][j] * gk[j];
     }
 
     alpha = Armijo();
-
     if (isZero(alpha))
       continue;
 
     normgrad = 0.0;
     normdeltax = 0.0;
     hy = 0.0;
+    yBy = 0.0;
 
     for (i = 0; i < numvar; i++) {
       h[i] = alpha * s[i];
@@ -58,81 +97,92 @@ int OptInfoBfgs::iteration(double* x0) {
       g0[i] = gk[i];
       hy += h[i] * y[i];
       normgrad += gk[i] * gk[i];
-      normdeltax += h[i]*h[i];
+      normdeltax += h[i] * h[i];
     }
+    normgrad = sqrt(normgrad);
 
     for (i = 0; i < numvar; i++) {
       By[i] = 0.0;
       for (j = 0; j < numvar; j++)
-	By[i] += Bk[i][j] * y[j];
+        By[i] += Bk[i][j] * y[j];
       yBy += y[i] * By[i];
     }
 
-    if (hy != 0 && yBy != 0)
-      for (i = 0; i < numvar; i++)
-	for (j = 0; j < numvar; j++)
-	  Bk[i][j] += h[i]*h[j]/hy - By[i]*By[j]/yBy + yBy*(h[i]/hy - By[i]/yBy)*(h[j]/hy - By[j]/yBy);
-
-    else
+    if ((isZero(hy)) || (isZero(yBy))) {
       check = 0;
-
-
-    if ((normgrad/(1.0 + abs(fk)) < bfgseps) && (normdeltax < bfgseps)) {
-      EcoSystem->setConvergeBFGS(1);
-      break;
+    } else {
+      temphy = 1.0 / hy;
+      tempyby = 1.0 / yBy;
+      for (i = 0; i < numvar; i++)
+        for (j = 0; j < numvar; j++)
+          Bk[i][j] += (h[i] * h[j] * temphy) - (By[i] * By[j] * tempyby)
+            + yBy * (h[i] * temphy - By[i] * tempyby) * (h[j] * temphy - By[j] * tempyby);
     }
-    cout << "\nNew optimum after " << FuncEval << " function evaluations, f(x) = " << fk << " at\n";
+
+    cout << "\nNew optimum after " << (FuncEval - offset) << " function evaluations, f(x) = " << fk << " at\n";
     for (i = 0; i < numvar; i++)
       cout << x[i] << sep;
     cout << endl;
-  }
 
-  EcoSystem->setFuncEvalBFGS(FuncEval - offset);
-  EcoSystem->setLikelihoodBFGS(fk);
-  return k;
+    //If the algorithm has met the convergence criteria, terminate the algorithm
+    if ((normgrad / (1.0 + absolute(fk)) < bfgseps) && (normdeltax < bfgseps)) {
+      cout << "\nStopping BFGS optimisation algorithm\n\n"
+        << "The optimisation stopped after " << (FuncEval - offset)
+        << " function evaluations (max " << bfgsiter << ")\nThe optimisation stopped because "
+        << "an optimum was found for this run\n";
+
+      fk = (*f)(x, numvar);
+      EcoSystem->setConvergeBFGS(1);
+      EcoSystem->setFuncEvalBFGS(FuncEval - offset);
+      EcoSystem->setLikelihoodBFGS(fk);
+      return 1;
+    }
+  }
 }
 
 void OptInfoBfgs::gradient(double* p, double fp) {
   int i, j;
-  double h = 0.00001, tmp[NUMVARS], tmp2[NUMVARS], temp;
+  double h;
+  double tmp[NUMVARS];
+
+  //h = 0.00001;
   for (i = 0; i < numvar; i++) {
-    for (j = 0; j < numvar; j++) {
+    for (j = 0; j < numvar; j++)
       tmp[j] = p[j];
-      tmp2[j] = p[j];
-    }
-    tmp[i] += h;
-    tmp2[i] -=h;
-    gk[i] = ((*f)(tmp,numvar) - fp) / (2 * h);
+
+    tmp[i] += gradacc;
+    gk[i] = ((*f)(tmp, numvar) - fp) / (2.0 * gradacc);
   }
 }
 
 double OptInfoBfgs::Armijo() {
-  double bn, fn = fk, sg = 0.0, df, tmp[NUMVARS];
-  int cond_satisfied = 0, i;
+  int i, cond;
+  double bn, fn, sg, df;
+  double tmp[NUMVARS];
+
+  cond = 0;
   bn = st;
+  fn = fk;
+  sg = 0.0;
   for (i = 0; i < numvar; i++)
-    sg += gk[i]*s[i];
-  //sg *= sigma;
-  
-  while(!cond_satisfied && bn > verysmall) {
+    sg += gk[i] * s[i];
+  sg *= sigma;
+
+  while (!cond && (bn > verysmall)) {
     for (i = 0; i < numvar; i++)
       tmp[i] = x[i] + bn * s[i];
-    
-    fn = (*f)(tmp,numvar);
 
-    
-    if((( fk - fn ) >= ( - bn * sigma * sg )) && ( fk > fn ))
-      cond_satisfied = 1;
+    fn = (*f)(tmp, numvar);
+    if (((fk - fn) >= (-bn * sg)) && (fk > fn))
+      cond = 1;
     else
       bn *= beta;
   }
-  
-  fk = fn;
-  gradient(tmp, fk);
-  if (cond_satisfied)
+
+  if ((cond) && (fn == fn)) {  //check for NaN
+    fk = fn;
+    gradient(tmp, fk);
     return bn;
-  else {
-    cerr << "alpha == 0\n";
-    return 0;
-  }
+  } else
+    return 0.0;
 }
