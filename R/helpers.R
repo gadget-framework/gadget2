001cf2e350df99f7c5ab64db52f85046fd123186
Ibrahim Umar <ibrahim.umar@imr.no>
2018-05-09 11:41:18 +0200

    Add new R function to run for a year and collect stats

--- R/helpers.R
+++ R/helpers.R
@@ -1,3 +1,107 @@
+processFleetStats <- function(fleets) {
+
+	ecoSystem <- getEcosystemInfo()
+	stocks <- ecoSystem[["stock"]]
+
+	getConsumption <- function(prey, fleetNo) {
+		stockNo <- getStockNo(prey)
+		tryCatch({
+			printPredatorPrey(fleetNo, stockNo)
+		}, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+	}
+
+	getStats <- function(x) {
+		fleetNo <- getFleetNo(x)
+		ret <- list()
+		catches <- lapply(stocks, getConsumption, fleetNo)
+		names(catches) <- stocks
+		ret[["catch"]] <- catches
+		return(ret)
+	}
+
+	stats <- lapply(fleets, getStats)
+	names(stats) <- fleets
+	return(stats)
+}
+
+processStockStats <- function(stocks) {
+
+	getStats <- function(x) {
+		stockNo <- getStockNo(x)
+		ret <- list()
+		ret[["stk"]] <- printStock(stockNo)
+		ret[["ssb"]] <- printSSB(stockNo)
+		ret[["rec"]] <- printRecruitment(stockNo)
+
+		return(ret)
+	}
+
+	stats <- lapply(stocks, getStats)
+	names(stats) <- stocks
+	return(stats)
+}
+
+
+# Run for a year and collect stats for stocks and fleets for that year
+runYear <- function() {
+
+	# Placeholders
+	stats <- list()
+	tmp <- list()
+
+	# Recursive list combining
+	combineStats <- function(x, path) {
+		path <- c(path, names(x))
+		y <- x[[1]]
+		if (class(y) == "list") {
+			for(i in names(y))
+				combineStats(y[i], path)
+		} else {
+			# Embed the matrix
+			# Must use eval and parse
+			eval(parse(text=paste0("xtemp <- tmp", paste0("[[\"", path, "\"]]", collapse=""))))
+			eval(parse(text=paste0("ztemp <- stats", paste0("[[\"", path, "\"]]", collapse=""))))
+
+			#print(paste(paste0("xtemp <- tmp", paste0("[[\"", path, "\"]]", collapse="")), "X rows", nrow(xtemp)))
+			#print(paste(paste0("ztemp <- stats", paste0("[[\"", path, "\"]]", collapse="")), "Z rows", nrow(ztemp)))
+
+			ztemp <- rbind(ztemp, xtemp)
+			#print(paste("comb rows ", nrow(ztemp)))
+			eval(parse(text=paste0("stats", paste0("[[\"", path, "\"]]", collapse="")," <<- ztemp")))
+			#print(paste("aggregated stats ", nrow(eval(parse(text=paste0("stats", paste0("[[\"", path, "\"]]", collapse="")))))))
+		}
+	}
+
+	ecoSystem <- getEcosystemInfo()
+	currentYear <- ecoSystem[["time"]][["currentYear"]]
+
+	while(TRUE) {
+		status <- stepSim()
+
+		# Get stats from all fleets
+		fleetStat <- processFleetStats(ecoSystem[["fleet"]])
+
+		# Get stats from all stocks
+		stockStat <- processStockStats(ecoSystem[["stock"]])
+
+		# Make temp list
+		tmp <- list(fleets=fleetStat, stocks=stockStat)
+
+		if(length(stats)==0) stats <- tmp
+		else {
+			combineStats(tmp["fleets"], c())
+			combineStats(tmp["stocks"], c())
+
+		}
+		# stop condition
+		if(status[["currentYear"]] > currentYear) break
+	}
+
+	print("End loop")
+
+	return(stats)
+}
+
 
 getFleetNo <- function(fleetName){
 	match(fleetName, getEcosystemInfo()$fleet)
