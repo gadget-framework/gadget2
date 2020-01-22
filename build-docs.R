b5dc96e7d1b83d54727166f3a0dc3a4aacd7cb71
Jamie Lentin <jm@lentin.co.uk>
2020-01-22 18:00:11 +0000

    build-docs: Add --serve option to auto-host content using servr

--- build-docs.R
+++ build-docs.R
@@ -6,11 +6,18 @@ args <- commandArgs(trailingOnly=TRUE)
 project_dir <- getwd()
 
 if (length(args) == 0) {
-    stop("Usage: ./build-docs.R (directory-to-build-into)")
+    stop("Usage: ./build-docs.R (directory-to-build-into | --serve)")
+}
+
+if (args[[1]] == '--serve') {
+    output_dir <- tempfile(fileext = ".build-docs")
+    serve_output <- TRUE
+} else {
+    output_dir <- args[[1]]
+    serve_output <- FALSE
 }
 
 # Work out output_dir
-output_dir <- args[[1]]
 dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
 output_dir <- normalizePath(output_dir)  # NB: The path has to exist first for this to work
 writeLines(paste("** Building documentation in", output_dir))
@@ -36,3 +43,7 @@ tryCatch({
         clean_envir = FALSE,
         output_dir = userguide_dir)
 }, finally = setwd(project_dir))
+
+if (serve_output) {
+    servr::httd(dir = output_dir, host = "0.0.0.0", port = 8000)
+}
