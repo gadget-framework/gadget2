088ca071802c386777282b3d4978c85600202395
Jamie Lentin <jm@lentin.co.uk>
2020-02-03 10:58:53 +0000

    src/stock: Fix -Wparentheses warnings

--- src/stock.cc
+++ src/stock.cc
@@ -200,10 +200,10 @@ Stock::Stock(CommentStream& infile, const char* givenname,
     } else if (strcasecmp(text, "fixedlength") == 0) {
       maturity = new MaturityB(subcomment, TimeInfo, keeper, minage, numage, areas, this->getName(), LgrpDiv);
 
-    } else if (strcasecmp(text, "newconstant") == 0 | strcasecmp(text, "constant") == 0) {
+    } else if ((strcasecmp(text, "newconstant") == 0) | (strcasecmp(text, "constant") == 0)) {
       maturity = new MaturityC(subcomment, TimeInfo, keeper, minage, numage, areas, this->getName(), LgrpDiv, 4);
 
-    } else if (strcasecmp(text, "newconstantweight") == 0 | strcasecmp(text, "constantweight") == 0) {
+    } else if ((strcasecmp(text, "newconstantweight") == 0) | (strcasecmp(text, "constantweight") == 0)) {
       maturity = new MaturityD(subcomment, TimeInfo, keeper, minage, numage, areas, this->getName(), LgrpDiv, 6, refweight);
 
     } else if (strcasecmp(text, "ageandlength") == 0) {
