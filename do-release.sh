0e0a1f8ff63944a0ec29da831f452ce1e1ed5352
Jamie Lentin <jm@lentin.co.uk>
2020-02-06 17:36:24 +0000

    do-release: Add CRAN URL for easy reference

--- do-release.sh
+++ do-release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Script to release package to CRAN.
-# Run ./do-release.sh (new-version), and upload the produced tarball
+# Run ./do-release.sh (new-version), and upload the produced tarball to:
+#     https://cran.r-project.org/submit.html
 set -eu
 
 NEW_VERSION="${1-}"
